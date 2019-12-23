2b770638a4f82fdb5199b22cbe835dbf28e158ba
Magnus Aarnes <magaar98@gmail.com>
2019-12-23 17:26:06 +0100

    test

--- data_vis/serial_read.py
+++ data_vis/serial_read.py
@@ -1,12 +1,12 @@
 import serial
 import numpy as np
 from matplotlib import pyplot as plt
-ser = serial.Serial(‘/dev/ttyACM0’, 9600)
+ser = serial.Serial('/dev/ttyACM0', 9600)
 
-plt.ion() # set plot to animated
+plt.ion()  # set plot to animated
 
 ydata = [0] * 50
-ax1=plt.axes()
+ax1 = plt.axes()
 
 # make plot
 line, = plt.plot(ydata)
@@ -14,14 +14,14 @@ plt.ylim([10,40])
 
 # start data collection
 while True:
-data = ser.readline().rstrip() # read data from serial
+    data = ser.readline().rstrip() # read data from serial
 # port and strip line endings
-if len(data.split(“.”)) == 2:
-ymin = float(min(ydata))-10
-ymax = float(max(ydata))+10
-plt.ylim([ymin,ymax])
-ydata.append(data)
-del ydata[0]
-line.set_xdata(np.arange(len(ydata)))
-line.set_ydata(ydata) # update the data
-plt.draw() # update the plot
+    if len(data.split(".")) == 2:
+        ymin = float(min(ydata))-10
+        ymax = float(max(ydata))+10
+        plt.ylim([ymin, ymax])
+        ydata.append(data)
+        del ydata[0]
+    line.set_xdata(np.arange(len(ydata)))
+    line.set_ydata(ydata) # update the data
+    plt.draw() # update the plot
